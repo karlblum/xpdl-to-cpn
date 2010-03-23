68d8b0e49c09e7f85bdebc2ec6e7d83b62ced797
karl.blum <karl.blum@dc0fc8c4-2f53-11df-bfeb-7b4c664f286a>
2010-03-23 22:02:37 +0000

    End of day. XPDL to CPN now mostly works. XOR, AND joins and splits are also working now. No simulation data mappings done yet.

--- Converter/src/ee/ut/converter/CPNConverter.java
+++ Converter/src/ee/ut/converter/CPNConverter.java
@@ -1,6 +1,7 @@
 package ee.ut.converter;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -8,16 +9,21 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import noNamespace.Cpnet;
+import noNamespace.Page;
 import noNamespace.WorkspaceElementsDocument;
 import ee.ut.model.bpmne.BPMNeProcess;
+import ee.ut.model.bpmne.BPMNeGateway.GWType;
 import ee.ut.model.sim.SimulationData;
 import ee.ut.model.xpdl2.Activities;
 import ee.ut.model.xpdl2.Activity;
 import ee.ut.model.xpdl2.Event;
 import ee.ut.model.xpdl2.PackageType;
 import ee.ut.model.xpdl2.ProcessType;
+import ee.ut.model.xpdl2.Route;
 import ee.ut.model.xpdl2.Transition;
+import ee.ut.model.xpdl2.TransitionRestrictions;
 import ee.ut.model.xpdl2.Transitions;
+import example.ExLucianoWrapper;
 
 /**
  * @author karl
@@ -29,6 +35,7 @@ public class CPNConverter {
 	File simDataFile;
 	WorkspaceElementsDocument cpnWorkspace;
 	Cpnet cpnet;
+	BPMNeProcess process;
 
 	/**
 	 * @param xpdlFile
@@ -53,29 +60,28 @@ public class CPNConverter {
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public Cpnet convert() {
+	public void convert() {
 
 		JAXBElement<PackageType> xpdlRoot = unMasrhall(xpdlFile,
 				"ee.ut.model.xpdl2");
 		JAXBElement<SimulationData> simDataRoot = unMasrhall(simDataFile,
 				"ee.ut.model.sim");
+		createBPMNeModel(xpdlRoot);
 
-		BPMNeProcess process = createBPMNeModel(xpdlRoot);
-
-		return cpnet;
 	}
 
 	/**
 	 * @param xpdlRoot
 	 * @return
 	 */
-	private BPMNeProcess createBPMNeModel(JAXBElement<PackageType> xpdlRoot) {
+	private void createBPMNeModel(JAXBElement<PackageType> xpdlRoot) {
 
 		ProcessType xpdlProcess = xpdlRoot.getValue().getWorkflowProcesses()
 				.getWorkflowProcess().get(0);
 
-		BPMNeProcess process = new BPMNeProcess(cpnet, xpdlProcess.getName(),
+		process = new BPMNeProcess(cpnet, xpdlProcess.getName(),
 				xpdlProcess.getName());
+		process.setNumberOfCases("start", "5"); //TODO: MANUAL
 
 		for (Object co : xpdlProcess.getContent()) {
 			if (co instanceof Activities) {
@@ -89,7 +95,7 @@ public class CPNConverter {
 			}
 		}
 
-		return process;
+		//process.setTransitionProbabilities("39", new Object[] {"5", 90, "41", 10}); //TODO: MANUAL
 	}
 
 	/**
@@ -105,7 +111,20 @@ public class CPNConverter {
 			System.out.println("DEBUG: Found start event (?)");
 		} else if (type == ActivityType.END) {
 			process.addTask(activity.getId(), activity.getName());
+			process.setExit(activity.getId());
 			System.out.println("DEBUG: Found end event (?)");
+		} else if (type == ActivityType.SPLIT_XOR){
+			process.addXORGateway(activity.getId(), activity.getName(), GWType.SPLIT);
+			System.out.println("DEBUG: Found XOR split (?)");
+		} else if (type == ActivityType.SPLIT_INC){
+			process.addANDGateway(activity.getId(), activity.getName(), GWType.SPLIT);
+			System.out.println("DEBUG: Found AND split (?)");
+		} else if (type == ActivityType.JOIN_INC){
+			process.addXORGateway(activity.getId(), activity.getName(), GWType.JOIN);
+			System.out.println("DEBUG: Found XOR join (?)");
+		} else if (type == ActivityType.JOIN_INC){
+			process.addANDGateway(activity.getId(), activity.getName(), GWType.JOIN);
+			System.out.println("DEBUG: Found AND join (?)");
 		} else {
 			process.addTask(activity.getId(), activity.getName());
 			System.out.println("DEBUG: Found intermediate event (?)");
@@ -135,18 +154,53 @@ public class CPNConverter {
 				} else if (((Event) aContent).getEndEvent() != null) {
 					return ActivityType.END;
 				}
+			} else if (aContent instanceof Route) {
+				if (((Route) aContent).getGatewayType().equals("Exclusive")){
+					if (isSplit(activity)){
+					return ActivityType.SPLIT_XOR;
+					} else {
+						return ActivityType.JOIN_XOR;
+					}
+				} else if (((Route) aContent).getGatewayType().equals("Inclusive")){
+					if (isSplit(activity)){
+						return ActivityType.SPLIT_INC;
+						} else {
+							return ActivityType.JOIN_INC;
+						}
+				}
+				
 			}
 		}
-		return ActivityType.INTERMEDIATE;
+		return ActivityType.TASK;
 	}
 
+	
+	/**
+	 * We assume that only split activity has transition restrictions. 
+	 * So this method determines whether the activity is split or join based 
+	 * on the existence of transistinrestriction elements 
+	 * 
+	 * @param activity
+	 * @return
+	 */
+	private static boolean isSplit(Activity activity){
+		for (Object aContent : activity.getContent()){
+			if( aContent instanceof TransitionRestrictions){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
 	/**
 	 * @author karl
 	 * 
 	 */
 	public enum ActivityType {
-		START, END, INTERMEDIATE
+		START, END, TASK, SPLIT_XOR, SPLIT_INC, JOIN_XOR, JOIN_INC
 	}
+	
 
 	/**
 	 * Method makes an in-memory representation of an input xml file
@@ -168,5 +222,17 @@ public class CPNConverter {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	public void saveToFile(File convertedCPNFile) {
+		for (Page p : cpnet.getPageArray())
+			ExLucianoWrapper.doLayouting(p);
+		
+		try {
+			cpnWorkspace.save(convertedCPNFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
