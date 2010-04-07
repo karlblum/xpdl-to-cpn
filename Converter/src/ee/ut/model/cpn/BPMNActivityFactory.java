43b199a75c0a7b5f01b1d2444a17cf16fcd408d4
karl.blum <karl.blum@dc0fc8c4-2f53-11df-bfeb-7b4c664f286a>
2010-04-07 20:38:16 +0000

    working on the new abstract pattern version

---
+++ Converter/src/ee/ut/model/cpn/BPMNActivityFactory.java
@@ -0,0 +1,11 @@
+package ee.ut.model.cpn;
+
+public class BPMNActivityFactory implements AbstractElementFactory {
+
+	@Override
+	public Object create(Object obj, CPNet cpnet, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Converter/src/ee/ut/model/cpn/BPMNFactory.java
@@ -0,0 +1,30 @@
+package ee.ut.model.cpn;
+
+import ee.ut.model.xpdl2.Transaction;
+
+public final class BPMNFactory implements AbstractElementFactory {
+	
+
+	AbstractElementFactory activityFactory = null;
+	AbstractElementFactory transitionFactory = null;
+	
+	public void registerActivityFactory(AbstractElementFactory f){
+		this.activityFactory = f;
+	}
+	
+	public void registerTransitionFactory(AbstractElementFactory f){
+		this.transitionFactory = f;
+	}
+
+	@Override
+	public Object create(Object obj, CPNet cpnet, String id) {
+		if (obj instanceof ee.ut.model.xpdl2.Activity)
+			return activityFactory.create(obj, cpnet, id);
+		else if (obj instanceof Transaction)
+			return transitionFactory.create(obj, cpnet, id);
+		else 
+			return null;
+	}
+	
+	
+}
