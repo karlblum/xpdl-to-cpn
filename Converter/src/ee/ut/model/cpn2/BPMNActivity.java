60eed6f5f71618890da616c9237cfd4624800cf4
karl.blum <karl.blum@dc0fc8c4-2f53-11df-bfeb-7b4c664f286a>
2010-04-10 10:13:59 +0000

    Generation from XPDL is working again. At the moment it generates XOR splits, AND joins and activities.

--- Converter/src/ee/ut/model/cpn2/BPMNActivity.java
+++ Converter/src/ee/ut/model/cpn2/BPMNActivity.java
@@ -1,48 +1,58 @@
 package ee.ut.model.cpn2;
 
-import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.xmlbeans.XmlString;
 
 import ee.ut.model.xpdl2.Activity;
 
 import noNamespace.Place;
+import noNamespace.Trans;
 
 public final class BPMNActivity extends BPMNElement {
 
-	private List<String> inputPlaceIds;
-	private List<String> outputPlaceIds;
+	private ArrayList<String> inputPlaceIds = new ArrayList<String>();
+	private ArrayList<String> outputPlaceIds = new ArrayList<String>();
 	private String transitionId;
 
 	public BPMNActivity(Process process, Object o) {
 		super(process);
 
 		Activity xpdlActivity = ((Activity) o);
-		setId(xpdlActivity.getId());
+		this.setId(xpdlActivity.getId());
 
 		// We assume that we only need a transition, because all the inputs and
 		// outputs can be generated dynamically.
-		transitionId = process.getCpnet().addTrans().getId();
+		Trans trans = process.getCpnet().addTrans();
+
+		// Set name
+		trans.addNewText().set(
+				XmlString.Factory.newValue(xpdlActivity.getName()));
+		transitionId = trans.getId();
+
+		// TODO:If this acctivity has start event, then we have to attach a
+		// generator
 
 	}
 
-	/**
-	 * We generate inputs dynamically. Every time we want to make a connection
-	 * between the gateway input and activity, we make a new input Place to both
-	 * ends. It generates kind of AND Joins.
-	 * 
-	 * @return
-	 */
-	public String makeInput() {
+	public Place makeInputPlace() {
 		Place p = process.getCpnet().addPlace();
+		p.getTypeArray()[0].getText().set(
+				XmlString.Factory.newValue(process.getCpnet()
+						.getFlowObjectType()));
 		inputPlaceIds.add(p.getId());
 		process.getCpnet().addArc(p.getId(), transitionId);
-		return p.getId();
+		return p;
 	}
 
-	public String makeOutput() {
+	public Place makeOutputPlace() {
 		Place p = process.getCpnet().addPlace();
+		p.getTypeArray()[0].getText().set(
+				XmlString.Factory.newValue(process.getCpnet()
+						.getFlowObjectType()));
 		outputPlaceIds.add(p.getId());
 		process.getCpnet().addArc(transitionId, p.getId());
-		return p.getId();
+		return p;
 	}
 
 }
