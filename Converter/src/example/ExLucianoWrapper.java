570b1fdc94de9c52bbe05c59a883a357631e9548
karl.blum <karl.blum@dc0fc8c4-2f53-11df-bfeb-7b4c664f286a>
2010-05-02 08:36:28 +0000

    Layouting is done through graphviz (dot)

--- Converter/src/example/ExLucianoWrapper.java
+++ Converter/src/example/ExLucianoWrapper.java
@@ -3,7 +3,12 @@ package example;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Frame;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -26,69 +31,15 @@ import ee.ut.old.model.bpmne.BPMNeProcess;
 
 public class ExLucianoWrapper {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		File f = new File("./files/cpn/blank.cpn");
-		WorkspaceElementsDocument doc = WorkspaceElementsDocument.Factory
-				.parse(f);
-		Cpnet cpnet = doc.getWorkspaceElements().getCpnet();
+	
+	public static void doLayouting(Page page) throws IOException {
 
-		BPMNeProcess proc = new BPMNeProcess(cpnet, "process", "process");
-
-		proc.addStartEvent("start", "Phone_Call");
-		proc.addTask("task_a", "Enough_Information_Available");
-
-		proc.setExit("task_a");
-		proc.addEdge("start", "task_a");
-
-		// proc.setTaskDDistribution("task_a", "normal(30.0,5.0)");
-
-		proc.setNumberOfCases("start", "5");
-		proc.setStartEventCDistribution("start", "round(erlang(10, 1.0))");
-
-		for (Page p : cpnet.getPageArray())
-			doLayouting(p);
-
-		f = new File("./files/cpn/ExLucianoWrapper.cpn");
-
-		doc.save(f);
-		System.out.println("Done");
-	}
-
-	public static void printNode(Node n) {
-		// System.out.printf("(%f, %f): [%f, %f]\n", n.getPosition().x,
-		// n.getPosition().y, n.getBoundingBox().height,
-		// n.getBoundingBox().width);
-	}
-
-	public static void doLayouting2(Page page) {
-		Graph graph = new Graph(true);
-		HashMap<String, Node> map = new HashMap<String, Node>();
-
-		JFrame f = new JFrame();
-		Font font = new Font("Serif", Font.PLAIN, 12);
-		FontMetrics fm = f.getFontMetrics(font);
-		for (Place p : page.getPlaceArray()) {
-			Node n = graph.createNode();
-			n.setLabel(p.getId());
-			n.setLabelPosition("IN");
-			n.setShape(Node.OVAL);
-			n.setBoundingBox(new DDimension(fm.stringWidth(n.getLabel()) + 60,
-					fm.getHeight() + 15));
-			map.put(p.getId(), n);
-		}
-
-		for (Trans t : page.getTransArray()) {
-			Node n = graph.createNode();
-			n.setLabel(t.getId());
-			n.setLabelPosition("IN");
-			n.setShape(Node.RECTANGLE);
-			n.setBoundingBox(new DDimension(fm.stringWidth(n.getLabel()) + 60,
-					fm.getHeight() + 15));
-			map.put(t.getId(), n);
-		}
+		FileWriter fstream = new FileWriter("./files/cpn/dot_input.txt");
+		BufferedWriter out = new BufferedWriter(fstream);
+		
+		HashMap<String, String[]> layoutData = new HashMap<String, String[]>();
+		
+		out.write("digraph G {nodesep = 1.75; ranksep=0.75; \n");
 
 		for (Arc arc : page.getArcArray()) {
 			String source, target;
@@ -99,75 +50,62 @@ public class ExLucianoWrapper {
 				source = arc.getTransend().getIdref();
 				target = arc.getPlaceend().getIdref();
 			}
-
-			graph.insertEdge(map.get(source), map.get(target));
+			out.write(source + "->" + target + "\n");	
 		}
-
-		TreeAlgorithm layouter = new TreeAlgorithm('l');
-		// CGDAlgorithm layouter = new CGDAlgorithm();
-
-		layouter.compute(graph, new GraphUpdate() {
-			double hSpacing = 20, vSpacing = 40;
-			double scale_ = 1.0;
-
-			public void center() {
-			}
-
-			public Frame getFrame() {
-				return null;
-			}
-
-			public double getHSpacing() {
-				return 1 / scale_ * hSpacing;
-			}
-
-			public double getVSpacing() {
-				return 1 / scale_ * vSpacing;
-			}
-
-			public Node getSelectedNode() {
-				return null;
-			}
-
-			public void scale(double scaleval) {
-			}
-
-			public void update(boolean adjust_bounds) {
-			}
-
-			public DRect windowRect() {
-				return null;
-			}
-
-		});
-
-		for (Node n : map.values())
-			printNode(n);
-
+			out.write("}");
+			out.close();
+		
+		Process pr = Runtime.getRuntime().exec("\"c:\\Program Files\\Graphviz2.26.3\\bin\\dot.exe\" .\\files\\cpn\\dot_input.txt");
+		
+		String line;
+		int lineNr = 0;
+		BufferedReader input =
+	        new BufferedReader
+	          (new InputStreamReader(pr.getInputStream()));
+	      while ((line = input.readLine()) != null) {
+	    	  lineNr++;
+	    	  if(lineNr>3 && !line.contains("->") && line.length() > 10){
+	  	        int posIDEnd = line.indexOf("[");
+	  	        int posCoordStart = line.indexOf("\"");
+	  	        int posCoordEnd = line.indexOf("\"", posCoordStart+1);
+	  	        String coords = line.substring(posCoordStart+1, posCoordEnd).trim();
+	  	        String tokenId = line.substring(0,posIDEnd).trim();
+	  	        layoutData.put(tokenId, new String[]{coords.split(",")[0],coords.split(",")[1]});
+	  	        System.out.println(tokenId);
+	    	  }
+	      }
+	      input.close();
+		
 		for (Place p : page.getPlaceArray()) {
-			Node n = map.get(p.getId());
+			if (layoutData.get(p.getId()) == null) continue;
+			
+			String xc = layoutData.get(p.getId())[0];
+			String yc = layoutData.get(p.getId())[1];
 			Posattr pos = Posattr.Factory.newInstance();
-			pos.setX(Double.toString(getRandom(1500)));
-			pos.setY(Double.toString(getRandom(1500)));
+			pos.setX(xc);
+			pos.setY(yc);
 			p.setPosattr(pos);
-			p.addNewEllipse().setH(Double.toString(n.getBoundingBox().height));
-			p.getEllipse().setW(Double.toString(n.getBoundingBox().width));
-
+			
+			p.addNewEllipse().setH("25");
+			p.getEllipse().setW("120");
+						
 			Posattr pos2 = Posattr.Factory.newInstance();
-			pos2.setX(Double.toString(n.getPosition().x
-					+ n.getBoundingBox().width / 2));
-			pos2.setY(Double.toString(n.getPosition().y
-					- n.getBoundingBox().height / 2));
+			
+			pos2.setX(Double.toString(Double.parseDouble(xc) + 60));
+			pos2.setY(Double.toString(Double.parseDouble(yc) + 20));
 			p.getTypeArray(0).addNewPosattr().set(pos2);
+		
 		}
 		for (Trans t : page.getTransArray()) {
-			Node n = map.get(t.getId());
+			String xc = layoutData.get(t.getId())[0];
+			String yc = layoutData.get(t.getId())[1];
 			Posattr pos = Posattr.Factory.newInstance();
-			pos.setX(Double.toString(getRandom(1500)));
-			pos.setY(Double.toString(getRandom(1500)));
+			pos.setX(xc);
+			pos.setY(yc);
 			t.setPosattr(pos);
-			t.addNewBox().setH(Double.toString(n.getBoundingBox().height));
-			t.getBox().setW(Double.toString(n.getBoundingBox().width));
+
+			t.addNewBox().setH("25");
+			t.getBox().setW("120");
 		}
 
 		for (Arc arc : page.getArcArray()) {
@@ -179,12 +117,16 @@ public class ExLucianoWrapper {
 				source = arc.getTransend().getIdref();
 				target = arc.getPlaceend().getIdref();
 			}
-			Node s = map.get(source), t = map.get(target);
+			Double xcSource =  Double.parseDouble(layoutData.get(source)[0]);
+			Double ycSource = Double.parseDouble(layoutData.get(source)[1]);
+			
+			Double xcTarget= Double.parseDouble(layoutData.get(target)[0]);
+			Double ycTarget = Double.parseDouble(layoutData.get(target)[1]);
 
-			double x = s.getPosition().x + 10
-					+ Math.abs((s.getPosition().x - t.getPosition().x) / 2.0);
-			double y = s.getPosition().y
-					- Math.abs((s.getPosition().y - t.getPosition().y) / 2.0);
+			double x = xcSource + 10
+					+ Math.abs((xcSource - xcTarget) / 2.0);
+			double y = ycSource
+					- Math.abs((ycSource - ycTarget) / 2.0);
 
 			if (arc.getAnnot() != null) {
 				Posattr pos = Posattr.Factory.newInstance();
@@ -193,143 +135,7 @@ public class ExLucianoWrapper {
 				arc.getAnnot().addNewPosattr().set(pos);
 			}
 		}
-	}
-
-	public static void doLayouting(Page page) {
-		Graph graph = new Graph(true);
-		HashMap<String, Node> map = new HashMap<String, Node>();
-
-		JFrame f = new JFrame();
-		Font font = new Font("Serif", Font.PLAIN, 12);
-		FontMetrics fm = f.getFontMetrics(font);
-		for (Place p : page.getPlaceArray()) {
-
-			// System.out.println("Place: " + p.getId());
-			Node n = graph.createNode();
-			n.setLabel(p.getId());
-			n.setLabelPosition("IN");
-			n.setShape(Node.OVAL);
-			n.setBoundingBox(new DDimension(fm.stringWidth(n.getLabel()) + 60,
-					fm.getHeight() + 15));
-			map.put(p.getId(), n);
-		}
-
-		for (Trans t : page.getTransArray()) {
-			// System.out.println("Transitions: " + t.getId() + "  " +
-			// t.getText().copy());
-			Node n = graph.createNode();
-			n.setLabel(t.getId());
-			n.setLabelPosition("IN");
-			n.setShape(Node.RECTANGLE);
-			n.setBoundingBox(new DDimension(fm.stringWidth(n.getLabel()) + 60,
-					fm.getHeight() + 15));
-			map.put(t.getId(), n);
-		}
-
-		for (Arc arc : page.getArcArray()) {
-			String source, target;
-			if (arc.getOrientation().equals("PtoT")) {
-				source = arc.getPlaceend().getIdref();
-				target = arc.getTransend().getIdref();
-			} else {
-				source = arc.getTransend().getIdref();
-				target = arc.getPlaceend().getIdref();
-			}
-			// System.out.printf("%s -> %s\n", source, target);
-
-			graph.insertEdge(map.get(source), map.get(target));
-		}
-
-		TreeAlgorithm layouter = new TreeAlgorithm('l');
-		// CGDAlgorithm layouter = new CGDAlgorithm();
-
-		layouter.compute(graph, new GraphUpdate() {
-			double hSpacing = 20, vSpacing = 40;
-			double scale_ = 1.0;
-
-			public void center() {
-			}
-
-			public Frame getFrame() {
-				return null;
-			}
-
-			public double getHSpacing() {
-				return 1 / scale_ * hSpacing;
-			}
-
-			public double getVSpacing() {
-				return 1 / scale_ * vSpacing;
-			}
-
-			public Node getSelectedNode() {
-				return null;
-			}
-
-			public void scale(double scaleval) {
-			}
-
-			public void update(boolean adjust_bounds) {
-			}
-
-			public DRect windowRect() {
-				return null;
-			}
-
-		});
-
-		for (Node n : map.values())
-			printNode(n);
-
-		for (Place p : page.getPlaceArray()) {
-			Node n = map.get(p.getId());
-			Posattr pos = Posattr.Factory.newInstance();
-			pos.setX(Double.toString(n.getPosition().x));
-			pos.setY(Double.toString(n.getPosition().y));
-			p.setPosattr(pos);
-			p.addNewEllipse().setH(Double.toString(n.getBoundingBox().height));
-			p.getEllipse().setW(Double.toString(n.getBoundingBox().width));
-
-			Posattr pos2 = Posattr.Factory.newInstance();
-			pos2.setX(Double.toString(n.getPosition().x
-					+ n.getBoundingBox().width / 2));
-			pos2.setY(Double.toString(n.getPosition().y
-					- n.getBoundingBox().height / 2));
-			p.getTypeArray(0).addNewPosattr().set(pos2);
-		}
-		for (Trans t : page.getTransArray()) {
-			Node n = map.get(t.getId());
-			Posattr pos = Posattr.Factory.newInstance();
-			pos.setX(Double.toString(n.getPosition().x));
-			pos.setY(Double.toString(n.getPosition().y));
-			t.setPosattr(pos);
-			t.addNewBox().setH(Double.toString(n.getBoundingBox().height));
-			t.getBox().setW(Double.toString(n.getBoundingBox().width));
-		}
-
-		for (Arc arc : page.getArcArray()) {
-			String source, target;
-			if (arc.getOrientation().equals("PtoT")) {
-				source = arc.getPlaceend().getIdref();
-				target = arc.getTransend().getIdref();
-			} else {
-				source = arc.getTransend().getIdref();
-				target = arc.getPlaceend().getIdref();
-			}
-			Node s = map.get(source), t = map.get(target);
-
-			double x = s.getPosition().x + 10
-					+ Math.abs((s.getPosition().x - t.getPosition().x) / 2.0);
-			double y = s.getPosition().y
-					- Math.abs((s.getPosition().y - t.getPosition().y) / 2.0);
-
-			if (arc.getAnnot() != null) {
-				Posattr pos = Posattr.Factory.newInstance();
-				pos.setX(Double.toString(x));
-				pos.setY(Double.toString(y));
-				arc.getAnnot().addNewPosattr().set(pos);
-			}
-		}
+		
 	}
 
 	public static double getRandom(int max) {
